c77f7e2395cdd079f629bb5dd218b267524858b4
omid <seyedomidnajmi@live.com>
2025-06-10 15:52:33 +0200

    feat: add cheatsheet.nvim plugin with Telescope integration and custom key mappings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,49 @@
 return {
+	-- Cheatsheet plugin - searchable cheatsheet using Telescope
+	{
+		"doctorfree/cheatsheet.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			local ctactions = require("cheatsheet.telescope.actions")
+			require("cheatsheet").setup({
+				bundled_cheatsheets = {
+					enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
+					disabled = { "nerd-fonts" },
+				},
+				bundled_plugin_cheatsheets = {
+					enabled = {
+						"auto-session",
+						"goto-preview",
+						"octo.nvim",
+						"telescope.nvim",
+						"vim-easy-align",
+						"vim-sandwich",
+						"gitsigns.nvim",
+					},
+					disabled = { "glow.nvim" },
+				},
+				include_only_installed_plugins = true,
+				telescope_mappings = {
+					["<CR>"] = ctactions.select_or_fill_commandline,
+					["<A-CR>"] = ctactions.select_or_execute,
+					["<C-Y>"] = ctactions.copy_cheat_value,
+					["<C-E>"] = ctactions.edit_user_cheatsheet,
+				},
+			})
+
+			-- Set keymap for cheatsheet
+			vim.keymap.set("n", "<leader>cc", "<cmd>Cheatsheet<CR>", {
+				desc = "Open cheatsheet",
+				silent = true
+			})
+		end,
+	},
+
 	-- OIL
 	{
 		"stevearc/oil.nvim",
