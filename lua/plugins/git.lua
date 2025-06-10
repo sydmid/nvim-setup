77e8a273d09b640d464a2839843b12773bd54937
omid <seyedomidnajmi@live.com>
2025-06-10 14:32:30 +0200

    feat: add Tab navigation for focusing preview window in github modified files Telescope

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -352,6 +352,24 @@ return {
 							end
 						end)
 
+						-- Tab navigation between prompt and preview
+						local focus_preview = function(prompt_bufnr)
+							local action_state = require("telescope.actions.state")
+							local picker = action_state.get_current_picker(prompt_bufnr)
+							local prompt_win = picker.prompt_win
+							local previewer = picker.previewer
+							local winid = previewer.state.winid
+							local bufnr = previewer.state.bufnr
+							vim.keymap.set("n", "<Tab>", function()
+								vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", prompt_win))
+							end, { buffer = bufnr })
+							vim.cmd(string.format("noautocmd lua vim.api.nvim_set_current_win(%s)", winid))
+						end
+
+						-- Bind Tab to focus preview window
+						map("n", "<Tab>", focus_preview)
+						map("i", "<Tab>", focus_preview)
+
 						-- q or Esc to close
 						map("n", "q", actions.close)
 						map("i", "<Esc>", actions.close)
