dea2ec214849fd567197bc0c0abc000cd058a4f4
omid <seyedomidnajmi@live.com>
2026-02-27 19:00:30 +0330

    feat: update Neovim configuration by removing unused plugins and adding new mini.nvim utilities

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -263,20 +263,20 @@ return {
       end,
       desc = "Commands",
     },
-    {
-      "<leader>sd",
-      function()
-        Snacks.picker.diagnostics()
-      end,
-      desc = "Diagnostics",
-    },
-    {
-      "<leader>sD",
-      function()
-        Snacks.picker.diagnostics_buffer()
-      end,
-      desc = "Buffer Diagnostics",
-    },
+    -- {
+    --   "<leader>sd",
+    --   function()
+    --     Snacks.picker.diagnostics()
+    --   end,
+    --   desc = "Diagnostics",
+    -- },
+    -- {
+    --   "<leader>sD",
+    --   function()
+    --     Snacks.picker.diagnostics_buffer()
+    --   end,
+    --   desc = "Buffer Diagnostics",
+    -- },
     {
       "<leader>sh",
       function()
