a3f7addee2d20b014f0346591111949431aba352
omid <seyedomidnajmi@live.com>
2026-03-11 23:04:14 +0330

    feat: using snacks file selector now

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -200,31 +200,20 @@ return {
       end,
       desc = "Find Files",
     },
-    -- {
-    --   "<D-p>",
-    --   function()
-    --     Snacks.picker.recent({
-    --       layout = "vscode",
-    --       exclude = {
-    --         ".git",
-    --         "*.meta",
-    --       },
-    --       ignored = false,
-    --     })
-    --   end,
-    --   desc = "Smart File Picker (git-aware)",
-    -- },
     {
       "<D-p>",
       function()
-        require("telescope").extensions.smart_open.smart_open {
-          cwd_only = true,
-          filename_first = false,
-        }
+        Snacks.picker.recent({
+          layout = "vscode",
+          exclude = {
+            ".git",
+            "*.meta",
+          },
+          ignored = false,
+        })
       end,
       desc = "Smart File Picker (git-aware)",
     },
-
     {
       "<leader>fc",
       function()
